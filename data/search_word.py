d6ceff31541150efb90d56ad7ac85216305cc696
ihgorek <aemen95@gmail.com>
2017-09-21 18:59:52 +0300

    some changes

--- data/search_word.py
+++ data/search_word.py
@@ -3,22 +3,6 @@ import random
 from pymongo import MongoClient
 
 
-#
-# def parse_message(text):
-#     res = re.search('\w{1,}@\w{1,}[.]\w{1,4}', text)
-#     if  res is None:
-#         return False
-#     else:
-#         return True
-#
-# def initial(messege):
-#     with open('data/orf_2.txt') as f:
-#         for st in f:
-#             if messege == st[0:len(messege)]:
-#                 return st[len(messege):len(messege)+1]
-#         else:
-#             return 'Давай что-нибудь другое'
-
 class BaldaGame:
     def __init__(self):
         self.word = ''
--- db and other/create_mongodb.py
+++ db and other/create_mongodb.py
@@ -1,24 +1,28 @@
-import pymongo
 from pymongo import MongoClient
 
 
-client = MongoClient('localhost', 27017)
+def main():
+    client = MongoClient('localhost', 27017)
 
-db = client.db_orf
-coll = db.orf_coll
+    db = client.db_orf
+    coll = db.orf_coll
 
-# Ввод нужных значений в нашу базу данных
-# coll.remove(None)
-with open('words.txt') as f:
-    id = 0
-    for l in f:
-        line = l.rstrip('\n')
-        d = { 'id' :str(id), 'word' : line}
-        coll.insert(d)
-        id += 1
-i = 0
-for men in coll.find():
-    i += 1
-    print(men)
-    if i > 40:
-        break
+    # Ввод нужных значений в нашу базу данных
+    # coll.remove(None)
+    with open('words.txt') as f:
+        id = 0
+        for l in f:
+            line = l.rstrip('\n')
+            d = { 'id' : str(id), 'word' : line}
+            coll.insert(d)
+            id += 1
+    i = 0
+    for men in coll.find():
+        i += 1
+        print(men)
+        if i > 40:
+            break
+
+
+if __name__ == '__main__':
+    main()
