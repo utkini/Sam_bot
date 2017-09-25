6300484ec6b1b6258f597957b574008b829e9bee
ihgorek <aemen95@gmail.com>
2017-09-25 12:55:55 +0300

    minor changes

--- db and other/train.py
+++ db and other/train.py
@@ -3,31 +3,26 @@ import pymongo
 
 import re
 
-# wrd = input('Введи слово \n')
-# print(word_search(wrd))
-
-# def sepr(mess):
-#     mess = mess.lower()
-#     for c in mess:
-#         if ord(c)<1040 or ord(c)>1103:
-#             return 'Вы ввели что-то не то'
-#     return  'Все хорошо'
+# ДЛЯ ОТСЕИВАНИЯ НЕНУЖНЫХ СЛОВ В ФАЙЛЕ ДЛЯ ИГРЫ "БАЛДА"
+# ==========================
+# words = open('orf_2.txt')
+# new_word = open('new_words.txt' , 'w')
+# w = "1"
+# for word in words:
+#     tmp = word[:len(word)-1]
+#     if w not in tmp:
+#         new_word.write(word)
+#         w = tmp
 #
-# mess = input()
-# print(sepr(mess))
+# new_word.close()
+# words.close()
 
-words = open('words.txt')
-new_word = open('new_words.txt' , 'w')
-w = "1"
-for word in words:
-    tmp = word[:len(word)-1]
-    if w not in tmp:
-        new_word.write(word)
-        w = tmp
 
-new_word.close()
-words.close()
-
+d = {'d': True,
+     'b': True,
+     'v': False}
+di = (map(lambda x: False, list(d.values())))
+print(di)
 
 
 
